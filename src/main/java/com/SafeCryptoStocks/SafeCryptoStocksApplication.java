dffc5e89d209173cc9b924df01caea316293a71d
Chinmaya Sahoo <cinshu39@gmail.com>
2024-12-23 20:44:31 +0530

    safe crypto stocks

---
+++ src/main/java/com/SafeCryptoStocks/SafeCryptoStocksApplication.java
@@ -0,0 +1,10 @@
+package com.SafeCryptoStocks;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCryptoStocksApplication {	
+	public static void main(String[] args) {
+		SpringApplication.run(SafeCryptoStocksApplication.class, args);
+	}
+}
